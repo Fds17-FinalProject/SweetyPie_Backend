5dd14e7d42b5ffd14a0a85c98d900250c77ce733
Tezla-Lee <tezla.lee@gmail.com>
2021-02-01 22:36:10 +0900

    faet: init AccommodationRepositoryTest

---
+++ src/test/java/com/mip/sharebnb/repository/AccommodationRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.mip.sharebnb.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccommodationRepositoryTest {
+
+    @Test
+    void findByCityContaining() {
+    }
+
+    @Test
+    void findByCityContainingOrGuContaining() {
+    }
+}
