9ce5e3773a5b7b71a4cdf06ecfe79798e4e46996
Tezla-Lee <tezla.lee@gmail.com>
2021-02-01 21:52:49 +0900

    feat: Add findByCityContaining

--- src/main/java/com/mip/sharebnb/repository/AccommodationRepository.java
+++ src/main/java/com/mip/sharebnb/repository/AccommodationRepository.java
@@ -1,7 +1,11 @@
 package com.mip.sharebnb.repository;
 
 import com.mip.sharebnb.model.Accommodation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AccommodationRepository extends CrudRepository<Accommodation, Long> {
+
+    Page<Accommodation> findByCityContaining(String searchKeyword, Pageable pageable);
 }
