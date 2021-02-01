72f09b66c85102f99ba2fe72495e4890cb94dd8a
Tezla-Lee <tezla.lee@gmail.com>
2021-02-01 11:50:36 +0900

    feat: Create getAccommodation

--- src/main/java/com/mip/sharebnb/controller/AccommodationController.java
+++ src/main/java/com/mip/sharebnb/controller/AccommodationController.java
@@ -1,4 +1,21 @@
 package com.mip.sharebnb.controller;
 
+import com.mip.sharebnb.model.Accommodation;
+import com.mip.sharebnb.repository.AccommodationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class AccommodationController {
+
+    private final AccommodationRepository accommodationRepository;
+
+    @GetMapping("/accommodation/{id}")
+    public Accommodation getAccommodation(@PathVariable Long id) {
+
+        return accommodationRepository.findById(id).orElse(Accommodation.emptyObject());
+    }
 }
