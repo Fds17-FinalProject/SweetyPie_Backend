00b4f5f5dae2e1e982de34abd8093253423ebfc9
Tezla-Lee <tezla.lee@gmail.com>
2021-02-01 22:35:12 +0900

    feat: Add findByCityContainingOrGuContaining
    At AccommodationRepository

--- src/main/java/com/mip/sharebnb/repository/AccommodationRepository.java
+++ src/main/java/com/mip/sharebnb/repository/AccommodationRepository.java
@@ -3,11 +3,11 @@ package com.mip.sharebnb.repository;
 import com.mip.sharebnb.model.Accommodation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccommodationRepository extends CrudRepository<Accommodation, Long> {
+public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
 
     Page<Accommodation> findByCityContaining(String searchKeyword, Pageable pageable);
 
-    Page<Accommodation> findByCityContainingOrGuContaining(String searchKeyword, Pageable pageable);
+    Page<Accommodation> findByCityContainingOrGuContaining(String city, String gu, Pageable pageable);
 }
