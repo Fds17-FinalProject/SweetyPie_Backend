98f343286e24dea5363117a59f3514753a370e05
limjoonchul <dlawnscjf511@naver.com>
2021-02-01 19:46:12 +0900

    feat: Add getReservations

--- src/main/java/com/mip/sharebnb/service/ReservationService.java
+++ src/main/java/com/mip/sharebnb/service/ReservationService.java
@@ -1,16 +1,33 @@
 package com.mip.sharebnb.service;
 
 import com.mip.sharebnb.dto.ReservationDto;
+import com.mip.sharebnb.model.Member;
+import com.mip.sharebnb.model.Reservation;
 import com.mip.sharebnb.repository.MemberRepository;
 import com.mip.sharebnb.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
-    private final MemberRepository memberRepository;
     private final ReservationRepository reservationRepository;
-    
 
+    public List<Reservation> getReservations(Long id) {
+        if (id == null){
+            return new ArrayList<>();
+        }
+        List<Reservation> reservations = reservationRepository.findReservationByMemberId(id);
+
+        if (reservations.isEmpty()){
+            return new ArrayList<>();
+        }
+
+        return reservations;
+
+    }
 }
