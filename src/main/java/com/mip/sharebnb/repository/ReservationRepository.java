0a6b6672a9b2a790f9fdbedffc10866effe5596a
limjoonchul <dlawnscjf511@naver.com>
2021-02-01 19:06:25 +0900

    feat: Add findReservationByMemberId

--- src/main/java/com/mip/sharebnb/repository/ReservationRepository.java
+++ src/main/java/com/mip/sharebnb/repository/ReservationRepository.java
@@ -2,6 +2,13 @@ package com.mip.sharebnb.repository;
 
 import com.mip.sharebnb.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    @Query("select r from Reservation as r where r.member.id = :member_id")
+    List<Reservation> findReservationByMemberId(@Param("member_id") Long id);
 }
