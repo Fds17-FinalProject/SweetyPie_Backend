d7010bff7b914337b8058deece413fa838c97c7c
limjoonchul <dlawnscjf511@naver.com>
2021-02-01 23:38:07 +0900

    feat: add getReservations

--- src/main/java/com/mip/sharebnb/controller/ReservationController.java
+++ src/main/java/com/mip/sharebnb/controller/ReservationController.java
@@ -1,14 +1,27 @@
 package com.mip.sharebnb.controller;
 
+import com.mip.sharebnb.dto.ReservationDto;
+import com.mip.sharebnb.model.Reservation;
+import com.mip.sharebnb.service.ReservationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class ReservationController {
 
+    private final ReservationService reservationService;
+
     @GetMapping("/reservation/{id}")
-    public void getReservations(@PathVariable Long id) {
-        
+    public List<Reservation> getReservations(@PathVariable Long id) {
+        if (id == null){
+            return new ArrayList<>();
+        }
+        return reservationService.getReservations(id);
     }
 }
