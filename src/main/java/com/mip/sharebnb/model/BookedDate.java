9a780631b83456a4bc04da40cbf013f735061544
Tezla-Lee <tezla.lee@gmail.com>
2021-02-02 10:23:50 +0900

    feat: Modify BookedDate
    Change AUTO -> IDENTITY

--- src/main/java/com/mip/sharebnb/model/BookedDate.java
+++ src/main/java/com/mip/sharebnb/model/BookedDate.java
@@ -17,7 +17,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class BookedDate {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
