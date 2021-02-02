2eabbe41d2ae4831bc11342f5b57a7c830590d7d
FoeverNa <foevna@naver.com>
2021-02-03 00:53:57 +0900

    feat: add configure method for permitAll Requests

--- src/main/java/com/mip/sharebnb/security/SecurityConfig.java
+++ src/main/java/com/mip/sharebnb/security/SecurityConfig.java
@@ -1,4 +1,16 @@
 package com.mip.sharebnb.security;
 
-public class SecurityConfig {
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**").permitAll();
+    }
 }
