479bb7d11da6b385f789c1762b010aa273111a6b
limjoonchul <dlawnscjf511@naver.com>
2021-02-02 23:45:04 +0900

    feat: Create ReservationDto

--- src/main/java/com/mip/sharebnb/dto/ReservationDto.java
+++ src/main/java/com/mip/sharebnb/dto/ReservationDto.java
@@ -1,4 +1,27 @@
 package com.mip.sharebnb.dto;
 
+import lombok.*;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReservationDto {
+
+    private String memberId;
+
+    private String accommodationId;
+
+    @NotEmpty
+    private String checkInDate;
+
+    @NotEmpty
+    private String checkoutDate;
+
+    @NotEmpty
+    private String guestNum;
+
+    @NotEmpty
+    private String totalPrice;
 }
