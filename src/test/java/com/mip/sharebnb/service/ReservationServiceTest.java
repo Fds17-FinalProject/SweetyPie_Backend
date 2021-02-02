436199e30d9726a8614ba2c57ef2af11912e2d06
limjoonchul <dlawnscjf511@naver.com>
2021-02-02 23:46:47 +0900

    test: Add updateReservation
    Add mockFindReservation and mockFindAccommodation

--- src/test/java/com/mip/sharebnb/service/ReservationServiceTest.java
+++ src/test/java/com/mip/sharebnb/service/ReservationServiceTest.java
@@ -1,25 +1,27 @@
 package com.mip.sharebnb.service;
 
+import com.mip.sharebnb.dto.ReservationDto;
 import com.mip.sharebnb.model.Accommodation;
+import com.mip.sharebnb.model.BookedDate;
 import com.mip.sharebnb.model.Member;
 import com.mip.sharebnb.model.Reservation;
 import com.mip.sharebnb.repository.AccommodationRepository;
-import com.mip.sharebnb.repository.MemberRepository;
 import com.mip.sharebnb.repository.ReservationRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ReservationServiceTest {
@@ -28,6 +30,9 @@ class ReservationServiceTest {
 
     @Mock
     private ReservationRepository reservationRepository;
+
+    @Mock
+    private AccommodationRepository accommodationRepository;
 
     @Test
     void getReservationByMemberId(){
@@ -49,6 +54,28 @@ class ReservationServiceTest {
         List<Reservation> reservations = reservationRepository.findReservationByMemberId(1L);
 
         assertThat(reservations.isEmpty()).isTrue();
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void updateReservation(){
+        when(reservationRepository.findById(1L)).thenReturn(mockFindReservation());
+        when(accommodationRepository.findById(1L)).thenReturn(mockFindAccommodation());
+
+        ReservationDto dto = new ReservationDto();
+        dto.setCheckInDate("2020-02-20");
+        dto.setCheckoutDate("2020-02-24");
+        dto.setGuestNum("3");
+        dto.setTotalPrice("30000");
+
+        Reservation reservation = reservationService.updateReservation(1L, dto);
+
+
+        verify(reservationRepository, times(1)).findById(1L);
+        verify(accommodationRepository, times(1)).findById(1L);
+        verify(reservationRepository, times(1)).save(any(Reservation.class));
+
     }
 
     private List<Reservation> mockReservation(){
@@ -73,4 +100,44 @@ class ReservationServiceTest {
         return reservations;
     }
 
+    private Optional<Reservation> mockFindReservation(){
+
+        Accommodation accommodation = new Accommodation();
+        accommodation.setId(1L);
+        accommodation.setBathroomNum(2);
+        accommodation.setBedroomNum(2);
+        accommodation.setAccommodationType("집전체");
+        accommodation.setBuildingType("아파트");
+
+        Reservation reservation = new Reservation();
+        LocalDate checkInDate = LocalDate.of(2020,2, 22);
+        LocalDate checkoutDate = LocalDate.of(2020,2, 24);
+        reservation.setCheckInDate(checkInDate);
+        reservation.setCheckoutDate(checkoutDate);
+        reservation.setTotalPrice(50000);
+        reservation.setGuestNum(5);
+        reservation.setAccommodation(accommodation);
+
+        return Optional.of(reservation);
+    }
+
+    private Optional<Accommodation> mockFindAccommodation(){
+        BookedDate bookedDate = new BookedDate();
+        bookedDate.setId(1L);
+        bookedDate.setDate(LocalDate.of(2020,2,22));
+
+        List<BookedDate> bookedDates = new ArrayList<>();
+        bookedDates.add(bookedDate);
+
+        Accommodation accommodation = new Accommodation();
+        accommodation.setId(1L);
+        accommodation.setBathroomNum(2);
+        accommodation.setBedroomNum(2);
+        accommodation.setAccommodationType("집전체");
+        accommodation.setBuildingType("게스트하우스");
+        accommodation.setBookedDates(bookedDates);
+        accommodation.setCapacity(4);
+
+        return Optional.of(accommodation);
+    }
 }
