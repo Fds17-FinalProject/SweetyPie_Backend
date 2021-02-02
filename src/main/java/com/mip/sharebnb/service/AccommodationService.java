776b88592d35c2b4fe6740795df0c6a1d03ec397
Tezla-Lee <tezla.lee@gmail.com>
2021-02-02 14:24:56 +0900

    feat: Add method at AccommodationService
    Add findAccommodationsByBookedDatesNotContaining

--- src/main/java/com/mip/sharebnb/service/AccommodationService.java
+++ src/main/java/com/mip/sharebnb/service/AccommodationService.java
@@ -1,12 +1,18 @@
 package com.mip.sharebnb.service;
 
 import com.mip.sharebnb.model.Accommodation;
+import com.mip.sharebnb.model.BookedDate;
 import com.mip.sharebnb.repository.AccommodationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +30,64 @@ public class AccommodationService {
         Pageable pageable = PageRequest.of(page, 10);
 
         return accommodationRepository.findByCityContainingOrGuContaining(searchKeyword, searchKeyword, pageable);
+    }
+
+    // 데이터 많아 질수록 성능 안 좋아짐. 임시용
+    public List<Accommodation> findAccommodationsByBookedDatesNotContaining(String searchKeyword,
+                                                                            LocalDate checkIn, LocalDate checkout,
+                                                                            int page) {
+        StringTokenizer st = new StringTokenizer(searchKeyword);
+
+        if (st.countTokens() > 1) {
+            while (st.hasMoreTokens()) {
+                searchKeyword = st.nextToken();
+            }
+        }
+
+        List<LocalDate> localDates = new ArrayList<>();
+        List<Accommodation> results = new ArrayList<>();
+
+        for (LocalDate date = checkIn; date.isBefore(checkout); date = date.plusDays(1)) {
+            localDates.add(date);
+        }
+
+        int count = 0;
+        int currentPage = 0;
+
+        while (count < (page + 1) * 10) {
+            Page<Accommodation> accommodations = findByCityContainingOrGuContaining(searchKeyword, currentPage++);
+
+
+            System.out.println("accommodations.getSize() : " + accommodations.getSize());
+
+            if (accommodations.getSize() == 0) {
+                return results;
+            }
+
+            for (Accommodation accommodation : accommodations) {
+                boolean flag = false;
+                List<BookedDate> bookedDates = accommodation.getBookedDates();
+
+                for (BookedDate bookedDate : bookedDates) {
+                    if (localDates.contains(bookedDate.getDate())) {
+                        flag = true;
+                        break;
+                    }
+                }
+
+                if (!flag) {
+                    count++;
+
+                    if (count >= page * 10) {
+                        results.add(accommodation);
+                        if (count == (page + 1) * 10) {
+                            return results;
+                        }
+                    }
+                }
+            }
+        }
+
+        return results;
     }
 }
