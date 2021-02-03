f43a1d513526727afb5a9f1ba1afe3bf1c5f0625
FoeverNa <foevna@naver.com>
2021-02-03 19:49:41 +0900

    feat: add MemberRole attributes

--- src/main/java/com/mip/sharebnb/model/MemberRole.java
+++ src/main/java/com/mip/sharebnb/model/MemberRole.java
@@ -1,4 +1,15 @@
 package com.mip.sharebnb.model;
 
+import lombok.Getter;
+
+@Getter
 public enum MemberRole {
+
+    ADMIN("ROLE_ADMIN"), Member("ROLE_MEMBER");
+
+    private final String roleName;
+
+    MemberRole(String roleName) {
+        this.roleName = roleName;
+    }
 }
