2fe46d3ab5a77d239c8db52c384712111ed241cb
FoeverNa <foevna@naver.com>
2021-02-03 15:08:55 +0900

    feat: add @Component annotation

--- src/main/java/com/mip/sharebnb/security/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/mip/sharebnb/security/jwt/JwtAccessDeniedHandler.java
@@ -2,11 +2,13 @@ package com.mip.sharebnb.security.jwt;
 
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
