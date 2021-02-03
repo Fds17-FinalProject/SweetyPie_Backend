917a016bb3e5ec308aa03ad54b664667d2f03737
FoeverNa <foevna@naver.com>
2021-02-04 00:10:32 +0900

    feat: add authorize to authorize member and return Jwt token

--- src/main/java/com/mip/sharebnb/service/MemberService.java
+++ src/main/java/com/mip/sharebnb/service/MemberService.java
@@ -1,4 +1,37 @@
 package com.mip.sharebnb.service;
 
+import com.mip.sharebnb.dto.LoginDto;
+import com.mip.sharebnb.dto.TokenDto;
+import com.mip.sharebnb.security.jwt.JwtFilter;
+import com.mip.sharebnb.security.jwt.TokenProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MemberService {
+
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    public MemberService(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
+        this.tokenProvider = tokenProvider;
+        this.authenticationManagerBuilder = authenticationManagerBuilder;
+    }
+
+    public String authorize(LoginDto loginDto) {
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return tokenProvider.createToken(authentication);
+    }
 }
