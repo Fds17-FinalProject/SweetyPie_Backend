55dac5aab350d8a968a074eb07bfc187614ee86c
FoeverNa <foevna@naver.com>
2021-02-03 16:23:04 +0900

    feat: add email, password

--- src/main/java/com/mip/sharebnb/dto/LoginDto.java
+++ src/main/java/com/mip/sharebnb/dto/LoginDto.java
@@ -1,4 +1,24 @@
 package com.mip.sharebnb.dto;
 
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginDto {
+
+    @Email
+    @NotNull
+    @Size(min =3, max = 50)
+    private String email;
+
+    @NotNull
+    @Size(min = 3, max = 30)
+    private String password;
 }
