71a136e2f12bd1b9de2f4ab8486674241167db0f
FoeverNa <foevna@naver.com>
2021-02-03 14:35:30 +0900

    feat: implements AccessDeniedHandler & Override handle
    sendError when token dose not have required authority

--- src/main/java/com/mip/sharebnb/security/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/mip/sharebnb/security/jwt/JwtAccessDeniedHandler.java
@@ -1,7 +1,18 @@
 package com.mip.sharebnb.security.jwt;
 
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class JwtAccessDeniedHandler {
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException {
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
 }
