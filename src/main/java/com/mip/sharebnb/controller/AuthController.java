5eda1d87c8d07c311b3be0b50e44a4a822bda7ee
FoeverNa <foevna@naver.com>
2021-02-04 00:24:43 +0900

    feat: add login api return TokenDto and httpHeaders

--- src/main/java/com/mip/sharebnb/controller/AuthController.java
+++ src/main/java/com/mip/sharebnb/controller/AuthController.java
@@ -1,4 +1,37 @@
 package com.mip.sharebnb.controller;
 
+import com.mip.sharebnb.dto.LoginDto;
+import com.mip.sharebnb.dto.TokenDto;
+import com.mip.sharebnb.security.jwt.JwtFilter;
+import com.mip.sharebnb.service.MemberService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
 public class AuthController {
+
+    private final MemberService memberService;
+
+    public AuthController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@Valid @RequestBody LoginDto loginDto) {
+
+        String jwt = memberService.authorize(loginDto);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, JwtFilter.HEADER_PREFIX + jwt);
+
+        return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
+    }
 }
