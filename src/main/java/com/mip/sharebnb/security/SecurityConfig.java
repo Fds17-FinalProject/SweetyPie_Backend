757431e9b3fae2b05e4fddee3b5cb38b3005f30d
FoeverNa <foevna@naver.com>
2021-02-03 14:48:20 +0900

    feat: SecurityConfig: add PasswordEncoder Bean

--- src/main/java/com/mip/sharebnb/security/SecurityConfig.java
+++ src/main/java/com/mip/sharebnb/security/SecurityConfig.java
@@ -1,11 +1,21 @@
 package com.mip.sharebnb.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
