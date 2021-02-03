c8af389e99ce2c13865fe22333a866c6ec9e40fa
FoeverNa <foevna@naver.com>
2021-02-03 11:32:33 +0900

    feat: add createToken to create JWT token
    param authentication is from authenticationManager
    
    jwt token claim include sub(email) auth(role)

--- src/main/java/com/mip/sharebnb/security/jwt/TokenProvider.java
+++ src/main/java/com/mip/sharebnb/security/jwt/TokenProvider.java
@@ -1,5 +1,56 @@
 package com.mip.sharebnb.security.jwt;
 
-public class TokenProvider  {
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+public class TokenProvider implements InitializingBean {
+
+    private static final String AUTHORITIES_KEY = "auth";
+
+    private final String secret;
+
+    private final long tokenValidityInMilliseconds;
+
+    private Key key;
+
+    public TokenProvider(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSeconds) {
+        this.secret = secret;
+        this.tokenValidityInMilliseconds = tokenValidityInSeconds * 1000;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String createToken(Authentication authentication) {
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = (new Date()).getTime();
+        Date validity = new Date(now + this.tokenValidityInMilliseconds);
+
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim(AUTHORITIES_KEY, authorities)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .setExpiration(validity)
+                .compact();
+    }
 }
