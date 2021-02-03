02760f8ed7a49596bbe20e6b318d0b94b900a673
FoeverNa <foevna@naver.com>
2021-02-03 12:55:15 +0900

    feat: add resolveToken to resolve token form http request header

--- src/main/java/com/mip/sharebnb/security/jwt/JwtFilter.java
+++ src/main/java/com/mip/sharebnb/security/jwt/JwtFilter.java
@@ -1,17 +1,31 @@
 package com.mip.sharebnb.security.jwt;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 public class JwtFilter extends GenericFilterBean {
 
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    public static final String HEADER_PREFIX = "Bearer ";
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(HEADER_PREFIX)) {
+            return bearerToken.substring(HEADER_PREFIX.length());
+        }
+        return null;
     }
 }
