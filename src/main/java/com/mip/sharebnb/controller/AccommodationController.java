98a8335da831f8761dadde3e6da0a61bcf0f7c47
Tezla-Lee <tezla.lee@gmail.com>
2021-02-03 10:33:18 +0900

    feat: Modify AccommodationController
    Add @RequestMapping("/api")

--- src/main/java/com/mip/sharebnb/controller/AccommodationController.java
+++ src/main/java/com/mip/sharebnb/controller/AccommodationController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class AccommodationController {
 
     private final AccommodationRepository accommodationRepository;
