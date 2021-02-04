cef8edd3d54c6814b7fba188acb042bebcfcfdd1
FoeverNa <foevna@naver.com>
2021-02-04 10:56:39 +0900

    feat: add RequestMapping to test Controller

--- src/main/java/com/mip/sharebnb/controller/helloController.java
+++ src/main/java/com/mip/sharebnb/controller/helloController.java
@@ -4,9 +4,11 @@ import com.mip.sharebnb.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class helloController {
 
     @Autowired
