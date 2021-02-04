ec49c77d7cea699fb2b47ab7a45ea4f855c358c2
FoeverNa <foevna@naver.com>
2021-02-04 15:42:15 +0900

    feat: Add Signup api

--- src/main/java/com/mip/sharebnb/controller/MemberController.java
+++ src/main/java/com/mip/sharebnb/controller/MemberController.java
@@ -1,7 +1,37 @@
 package com.mip.sharebnb.controller;
 
+import com.mip.sharebnb.dto.MemberDto;
+import com.mip.sharebnb.model.Member;
+import com.mip.sharebnb.service.MemberService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequestMapping("/api")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/member")
+    public ResponseEntity<MemberDto> signup(
+            @Valid @RequestBody MemberDto memberDto) {
+
+        Member member = memberService.signup(memberDto);
+        MemberDto result = MemberDto.builder()
+                .email(member.getEmail())
+                .name(member.getName())
+                .contact(member.getContact())
+                .build();
+
+        return ResponseEntity.ok(result);
+    }
 }
