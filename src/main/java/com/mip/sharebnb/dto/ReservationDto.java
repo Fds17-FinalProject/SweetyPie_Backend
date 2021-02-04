a31afcbf3970f1a24ff8193cdee0c20f9b004a3c
limjoonchul <dlawnscjf511@naver.com>
2021-02-04 12:38:54 +0900

    feat: Modify ReservationDto

--- src/main/java/com/mip/sharebnb/dto/ReservationDto.java
+++ src/main/java/com/mip/sharebnb/dto/ReservationDto.java
@@ -1,10 +1,12 @@
 package com.mip.sharebnb.dto;
 
 import com.mip.sharebnb.model.Accommodation;
+import com.mip.sharebnb.model.AccommodationPicture;
 import com.mip.sharebnb.model.Reservation;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -12,6 +14,7 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 public class ReservationDto {
 
+    @NonNull
     private Reservation reservation;
 
     private AccommodationDto accommodationDto;
