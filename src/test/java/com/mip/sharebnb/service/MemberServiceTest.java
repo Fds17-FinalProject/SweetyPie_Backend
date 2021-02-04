bb6b2116b43de82227f9aba342ecac30c4dc2a4e
FoeverNa <foevna@naver.com>
2021-02-05 00:49:00 +0900

    test : add signupTest

--- src/test/java/com/mip/sharebnb/service/MemberServiceTest.java
+++ src/test/java/com/mip/sharebnb/service/MemberServiceTest.java
@@ -4,18 +4,20 @@ import com.mip.sharebnb.dto.MemberDto;
 import com.mip.sharebnb.model.Member;
 import com.mip.sharebnb.model.MemberRole;
 import com.mip.sharebnb.repository.MemberRepository;
+import com.mip.sharebnb.security.jwt.TokenProvider;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class MemberServiceTest {
