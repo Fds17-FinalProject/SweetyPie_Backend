2df3d50a3c93fc306707ee1317a037c6735b209c
FoeverNa <foevna@naver.com>
2021-02-04 10:01:31 +0900

    feat: add @Component annotation to use CustomUserDetailsService

--- src/main/java/com/mip/sharebnb/security/service/CustomUserDetailsService.java
+++ src/main/java/com/mip/sharebnb/security/service/CustomUserDetailsService.java
@@ -9,12 +9,13 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@Component("userDetailsService")
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
