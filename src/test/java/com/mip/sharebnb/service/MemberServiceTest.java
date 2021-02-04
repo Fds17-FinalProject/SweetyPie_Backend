df530bccd5d5f291b1f96ed020b2344e052f4c8f
FoeverNa <foevna@naver.com>
2021-02-05 00:02:39 +0900

    test : Add signup test

--- src/test/java/com/mip/sharebnb/service/MemberServiceTest.java
+++ src/test/java/com/mip/sharebnb/service/MemberServiceTest.java
@@ -1,13 +1,17 @@
 package com.mip.sharebnb.service;
 
+import com.mip.sharebnb.dto.MemberDto;
 import com.mip.sharebnb.model.Member;
+import com.mip.sharebnb.model.MemberRole;
 import com.mip.sharebnb.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,8 +26,11 @@ class MemberServiceTest {
     @Mock
     private MemberRepository memberRepository;
 
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
     @Test
-    void getMember() {
+    void getMemberTest() {
         Member member = Member.builder()
                 .id(1L)
                 .email("member@mail.com")
@@ -33,6 +40,33 @@ class MemberServiceTest {
                 .thenReturn(Optional.ofNullable(member));
 
         Member result = memberService.getMember(1L);
+
+        assertThat(result.getEmail()).isEqualTo("member@mail.com");
+    }
+
+    @Test
+    void signupTest() {
+
+        Member member = Member.builder()
+                .email("member@mail.com")
+                .name("member")
+                .birthDate(LocalDate.now())
+                .contact("011-111-1111")
+                .role(MemberRole.MEMBER)
+                .build();
+
+        when(memberRepository.save(member))
+                .thenReturn(member);
+
+        MemberDto memberDto = MemberDto.builder()
+                .email("member@mail.com")
+                .password("member")
+                .name("member")
+                .birthDate(LocalDate.now())
+                .contact("011-111-1111")
+                .build();
+
+        Member result = memberService.signup(memberDto);
 
         assertThat(result.getEmail()).isEqualTo("member@mail.com");
     }
