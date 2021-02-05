3896d6a0e1931d96d371cb081cf8b6db80242909
FoeverNa <foevna@naver.com>
2021-02-05 10:56:37 +0900

    test: add getMemberTest

---
+++ src/test/java/com/mip/sharebnb/controller/MemberControllerTest.java
@@ -0,0 +1,39 @@
+package com.mip.sharebnb.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(properties = "spring.config.location="
+        + "classpath:application.yml,"
+        + "classpath:datasource.yml")
+class MemberControllerTest {
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void before(WebApplicationContext wac) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .alwaysDo(print())
+                .build();
+    }
+
+    @Test
+    void getMemberTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/member/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.email").value("member1@mail.com"));
+    }
+
+}
