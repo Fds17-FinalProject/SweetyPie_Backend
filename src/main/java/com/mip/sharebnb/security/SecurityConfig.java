3329f1aeb20c8837dac87efd3b88800d5388ec5b
FoeverNa <foevna@naver.com>
2021-02-05 11:17:30 +0900

    fix: remove annotation processing

--- src/main/java/com/mip/sharebnb/security/SecurityConfig.java
+++ src/main/java/com/mip/sharebnb/security/SecurityConfig.java
@@ -1,60 +1,58 @@
-//package com.mip.sharebnb.security;
-//
-//import com.mip.sharebnb.security.jwt.JwtAccessDeniedHandler;
-//import com.mip.sharebnb.security.jwt.JwtAuthenticationEntryPoint;
-//import com.mip.sharebnb.security.jwt.JwtSecurityConfig;
-//import com.mip.sharebnb.security.jwt.TokenProvider;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
-//@RequiredArgsConstructor
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    private final TokenProvider tokenProvider;
-//    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-//    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
-//
-//    // 강사님께 질문한뒤 삭제
-//
-////    public SecurityConfig(TokenProvider tokenProvider,
-////                          JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
-////                          JwtAccessDeniedHandler jwtAccessDeniedHandler) {
-////        this.tokenProvider = tokenProvider;
-////        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
-////        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
-////    }
-//
-//
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .csrf().disable()
-//
-//                .exceptionHandling()
-//                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-//                .accessDeniedHandler(jwtAccessDeniedHandler)
-//
-//                .and()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/api/**").permitAll()
-//                .anyRequest().authenticated()
-//
-//                .and()
-//                .apply(new JwtSecurityConfig(tokenProvider));
-//
+package com.mip.sharebnb.security;
+
+import com.mip.sharebnb.security.jwt.JwtAccessDeniedHandler;
+import com.mip.sharebnb.security.jwt.JwtAuthenticationEntryPoint;
+import com.mip.sharebnb.security.jwt.JwtSecurityConfig;
+import com.mip.sharebnb.security.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    // 강사님께 질문한뒤 삭제
+
+//    public SecurityConfig(TokenProvider tokenProvider,
+//                          JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+//                          JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+//        this.tokenProvider = tokenProvider;
+//        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+//        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
 //    }
-//}
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/**").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+
+    }
+}
