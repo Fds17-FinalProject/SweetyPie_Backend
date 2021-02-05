da022fc2049ceb7f6a18748afdfce3ead4f64116
limjoonchul <dlawnscjf511@naver.com>
2021-02-05 16:41:04 +0900

    feat: Add updateReservation

--- src/main/java/com/mip/sharebnb/service/ReservationService.java
+++ src/main/java/com/mip/sharebnb/service/ReservationService.java
@@ -2,27 +2,30 @@ package com.mip.sharebnb.service;
 
 import com.mip.sharebnb.dto.AccommodationDto;
 import com.mip.sharebnb.dto.ReservationDto;
+import com.mip.sharebnb.model.BookedDate;
 import com.mip.sharebnb.model.Reservation;
-import com.mip.sharebnb.repository.AccommodationRepository;
 import com.mip.sharebnb.repository.ReservationRepository;
 
+import com.mip.sharebnb.repository.dynamic.DynamicReservationRepository;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
+
+    private final DynamicReservationRepository dynamicReservationRepository;
+
     private final ReservationRepository reservationRepository;
-    private final AccommodationRepository accommodationRepository;
 
     public List<ReservationDto> getReservations(Long memberId) {
-        if (memberId == null){
+        if (memberId == null) {
             return new ArrayList<>();
         }
         List<Reservation> reservations = reservationRepository.findReservationByMemberId(memberId);
@@ -43,29 +46,39 @@ public class ReservationService {
         return reservationDtoList;
     }
 
-    public Reservation updateReservation(Long id, ReservationDto reservationDto) {
-
-        List<LocalDate> dates = setDate(reservationDto.getCheckInDate(), reservationDto.getCheckoutDate());
-
-        return null;
-    }
-
-    private List<LocalDate> setDate(LocalDate checkInDate, LocalDate checkoutDate){
-        List<LocalDate> dates = new ArrayList<>();
-        for (LocalDate date = checkInDate; date.isBefore(checkoutDate); date.plusDays(1)) {
-            dates.add(date);
-        }
-
-        return dates;
-    }
-
     public AccommodationDto mappingAccommodationDto(Reservation reservation) {
+
         AccommodationDto accommodationDto = new AccommodationDto();
+
         accommodationDto.setCity(reservation.getAccommodation().getCity());
         accommodationDto.setGu(reservation.getAccommodation().getGu());
         accommodationDto.setAccommodationPictures(reservation.getAccommodation().getAccommodationPictures());
 
         return accommodationDto;
+    }
 
+    @Transactional
+    public Reservation updateReservation(Long id, ReservationDto reservationDto) {
+
+        Optional<Reservation> findReservation = Optional.of(reservationRepository.findById(id).orElse(new Reservation()));
+
+        Reservation reservation = findReservation.get();
+
+        System.out.println("reservation >>>" + reservation.toString());
+
+        // accommodation의 날짜를 비교해야 함
+        List<BookedDate> reservations = dynamicReservationRepository.findByReservationIdAndDate(id, reservation.getAccommodation().getId(), reservationDto.getCheckInDate(), reservationDto.getCheckoutDate());
+
+        if (reservations.isEmpty()) {
+            reservation.setCheckInDate(reservationDto.getCheckInDate());
+            reservation.setCheckoutDate(reservationDto.getCheckoutDate());
+            reservation.setGuestNum(reservationDto.getGuestNum());
+            reservation.setTotalPrice(reservationDto.getTotalPrice());
+
+            Reservation result = reservationRepository.save(reservation);
+            return result;
+        }
+
+        return new Reservation();
     }
 }
