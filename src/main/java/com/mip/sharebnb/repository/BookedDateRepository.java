9b952300ef0cbbd50606835efc28675dd223057c
Tezla-Lee <tezla.lee@gmail.com>
2021-02-05 17:53:14 +0900

    feat: Update BookedDateRepository
    Add findBookedDatesByAccommodation_Id for test...

--- src/main/java/com/mip/sharebnb/repository/BookedDateRepository.java
+++ src/main/java/com/mip/sharebnb/repository/BookedDateRepository.java
@@ -3,6 +3,8 @@ package com.mip.sharebnb.repository;
 import com.mip.sharebnb.model.BookedDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookedDateRepository extends JpaRepository<BookedDate, Long> {
-    
+    List<BookedDate> findBookedDatesByAccommodation_Id(Long AccommodationId);
 }
