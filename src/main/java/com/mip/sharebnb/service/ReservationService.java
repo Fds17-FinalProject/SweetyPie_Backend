ecc8cbbe450b3a9f699cea4ca3670b2b8532a261
limjoonchul <dlawnscjf511@naver.com>
2021-02-06 14:16:50 +0900

    feat: Add insertReservation

--- src/main/java/com/mip/sharebnb/service/ReservationService.java
+++ src/main/java/com/mip/sharebnb/service/ReservationService.java
@@ -17,6 +17,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.crypto.spec.PSource;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -29,8 +32,10 @@ public class ReservationService {
     private final ReservationRepository reservationRepository;
     private final MemberRepository memberRepository;
     private final AccommodationRepository accommodationRepository;
-
     private final BookedDateRepository bookedDateRepository;
+
+    private Long code = 100000000L;
+    private LocalDate saveDate;
 
     public List<ReservationDto> getReservations(Long memberId) {
         if (memberId == null) {
@@ -54,20 +59,41 @@ public class ReservationService {
         return reservationDtoList;
     }
 
+    @Transactional
     public Reservation insertReservation(ReservationDto reservationDto) {
-
+        System.out.println("memeberId " + reservationDto.getMemberId() + " accommodationId" + reservationDto.getAccommodationId());
         Optional<Member> optionalMember = Optional.of(memberRepository.findById(reservationDto.getMemberId()).orElse(new Member()));
         Member member = optionalMember.get();
 
         Optional<Accommodation> optionalAccommodation = Optional.of(accommodationRepository.findById(reservationDto.getAccommodationId()).orElse(new Accommodation()));
-
         Accommodation accommodation = optionalAccommodation.get();
 
-        List<BookedDate> checkDuplicateDate = dynamicReservationRepository.findByReservationIdAndDate(reservationDto.getReservationId(), reservationDto.getAccommodationId(), reservationDto.getCheckInDate(), reservationDto.getCheckoutDate());
+        List<BookedDate> bookedDates = new ArrayList<>();
 
-        Reservation reservation = new Reservation();
-//        if (checkDuplicateDate.isEmpty()) {
-//        }
+        for (LocalDate date = reservationDto.getCheckInDate(); date.isBefore(reservationDto.getCheckoutDate()); date = date.plusDays(1)) {
+            bookedDates.add(saveBookedDate(date, accommodation));
+        }
+
+        List<BookedDate> checkDuplicateDate = dynamicReservationRepository.findByReservationIdAndDate(reservationDto.getAccommodationId(), reservationDto.getCheckInDate(), reservationDto.getCheckoutDate());
+        System.out.println(" >>> " + checkDuplicateDate.isEmpty());
+
+        if (checkDuplicateDate.isEmpty()) {
+            Reservation buildReservation = Reservation.builder()
+                    .checkInDate(reservationDto.getCheckInDate())
+                    .checkoutDate(reservationDto.getCheckoutDate())
+                    .guestNum(reservationDto.getGuestNum())
+                    .totalPrice(reservationDto.getTotalPrice())
+                    .isCanceled(false)
+                    .paymentDate(LocalDate.now())
+                    .member(member)
+                    .accommodation(accommodation)
+                    .bookedDates(bookedDates)
+                    .reservationCode(setReservationCode())
+                    .build();
+
+            System.out.println(setReservationCode());
+            return reservationRepository.save(buildReservation);
+        }
 
         return new Reservation();
     }
@@ -109,7 +135,7 @@ public class ReservationService {
     }
 
 
-    public AccommodationDto mappingAccommodationDto(Reservation reservation) {
+    private AccommodationDto mappingAccommodationDto(Reservation reservation) {
 
         AccommodationDto accommodationDto = new AccommodationDto();
 
@@ -118,5 +144,33 @@ public class ReservationService {
         accommodationDto.setAccommodationPictures(reservation.getAccommodation().getAccommodationPictures());
 
         return accommodationDto;
+    }
+
+    private BookedDate saveBookedDate(LocalDate date, Accommodation accommodation) {
+        BookedDate bookedDate = BookedDate.builder()
+                .date(date)
+                .accommodation(accommodation)
+                .build();
+        bookedDateRepository.save(bookedDate);
+
+        return bookedDate;
+    }
+
+    private String setReservationCode(){
+        LocalDate today = LocalDate.now();
+
+        if (saveDate.equals(today)){
+            code += 1;
+        } else {
+            saveDate = today;
+            code = 10000000L;
+        }
+
+        String nowDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        StringBuilder sb = new StringBuilder();
+        sb.append("Num").append(nowDate).append(String.valueOf(code));
+
+        return sb.toString();
+
     }
 }
