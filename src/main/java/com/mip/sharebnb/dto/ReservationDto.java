c010dcdba3f1f3e93ca80ab7931cd4c2376b0f62
limjoonchul <dlawnscjf511@naver.com>
2021-02-06 14:16:17 +0900

    feat: Add @Builder @DataTimeFormat

--- src/main/java/com/mip/sharebnb/dto/ReservationDto.java
+++ src/main/java/com/mip/sharebnb/dto/ReservationDto.java
@@ -1,10 +1,11 @@
 package com.mip.sharebnb.dto;
 
 import lombok.*;
-
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
+@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,9 +19,11 @@ public class ReservationDto {
     private Long reservationId;
 
     @NonNull
+    @DateTimeFormat // 형태 지정하는것 다시 보기
     private LocalDate checkInDate;
 
     @NonNull
+    @DateTimeFormat
     private LocalDate checkoutDate;
 
     private int guestNum;
