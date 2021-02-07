0f98489ccc4ec0b08a5f6df1191bc66d9be0aea8
Tezla-Lee <tezla.lee@gmail.com>
2021-02-07 14:40:45 +0900

    feat: Update Bookmark
    Change GenerationType: AUTO -> IDENTITY

--- src/main/java/com/mip/sharebnb/model/Bookmark.java
+++ src/main/java/com/mip/sharebnb/model/Bookmark.java
@@ -21,7 +21,7 @@ import javax.persistence.ManyToOne;
 public class Bookmark {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ToString.Exclude
