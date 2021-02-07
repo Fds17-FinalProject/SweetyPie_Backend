9aacd2bde60941fdddc02d8d8551969a936c3831
Tezla-Lee <tezla.lee@gmail.com>
2021-02-07 14:41:12 +0900

    feat: Update DynamicAccommodationRepositoryTest
    Delete Bookmark list

--- src/test/java/com/mip/sharebnb/repository/dynamic/DynamicAccommodationRepositoryTest.java
+++ src/test/java/com/mip/sharebnb/repository/dynamic/DynamicAccommodationRepositoryTest.java
@@ -119,7 +119,7 @@ class DynamicAccommodationRepositoryTest {
     }
 
     private Accommodation givenAccommodation() {
-        Accommodation accommodation = new Accommodation(null, "대구광역시", "수성구", "대구광역시 수성구 xx로", "원룸", 1, 1, 1, 40000, 2, "010-1234-5678", 36.141f, 126.531f, "마포역 1번 출구 앞", "버스 7016", "깨끗해요", "착해요", 4.56f, 125, "전체", "원룸", "이재복", 543, null, null, null, null, null);
+        Accommodation accommodation = new Accommodation(null, "대구광역시", "수성구", "대구광역시 수성구 xx로", "원룸", 1, 1, 1, 40000, 2, "010-1234-5678", 36.141f, 126.531f, "마포역 1번 출구 앞", "버스 7016", "깨끗해요", "착해요", 4.56f, 125, "전체", "원룸", "이재복", 543, null, null, null, null);
 
         return accommodation;
     }
