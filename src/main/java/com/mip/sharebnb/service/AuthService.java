e7675a0edcb44e19577a9f80a9fea39b561da6b4
FoeverNa <foevna@naver.com>
2021-02-07 10:00:04 +0900

    feat: Add GetGoogleUserInfo

--- src/main/java/com/mip/sharebnb/service/AuthService.java
+++ src/main/java/com/mip/sharebnb/service/AuthService.java
@@ -1,13 +1,26 @@
 package com.mip.sharebnb.service;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.mip.sharebnb.dto.GoogleLoginRequestDto;
+import com.mip.sharebnb.dto.GoogleLoginResponseDto;
 import com.mip.sharebnb.dto.LoginDto;
 import com.mip.sharebnb.security.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +28,13 @@ public class AuthService {
 
     private final TokenProvider tokenProvider;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    final static String GOOGLE_TOKEN_BASE_URL = "https://oauth2.googleapis.com/token";
+
+    @Value("${google.client_id}")
+    String clientId;
+    @Value("${google.client_secret}")
+    String clientSecret;
 
     public String login(LoginDto loginDto) {
 
@@ -26,4 +46,44 @@ public class AuthService {
 
         return tokenProvider.createToken(authentication);
     }
+
+    public Map<String, String> getGoogleUserInfo(String authCode) throws JsonProcessingException {
+
+        //HTTP Request를 위한 RestTemplate
+        RestTemplate restTemplate = new RestTemplate();
+
+        //Google OAuth Access Token 요청을 위한 파라미터 세팅
+        GoogleLoginRequestDto googleOAuthRequestParam = GoogleLoginRequestDto
+                .builder()
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .code(authCode)
+                .redirectUri("http://localhost:8080/api/login/google")
+                .grantType("authorization_code").build();
+        System.out.println(authCode);
+        //JSON 파싱을 위한 기본값 세팅
+        //요청시 파라미터는 스네이크 케이스로 세팅되므로 Object mapper에 미리 설정해준다.
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        //AccessToken 발급 요청
+        ResponseEntity<String> resultEntity = restTemplate.postForEntity(GOOGLE_TOKEN_BASE_URL, googleOAuthRequestParam, String.class);
+
+        //Token Request
+        GoogleLoginResponseDto result = mapper.readValue(resultEntity.getBody(), new TypeReference<GoogleLoginResponseDto>() {
+        });
+
+        System.out.println(resultEntity.getBody());
+
+        //ID Token만 추출 (사용자의 정보는 jwt로 인코딩 되어있다)
+        String jwtToken = result.getIdToken();
+        String requestUrl = UriComponentsBuilder.fromHttpUrl("https://oauth2.googleapis.com/tokeninfo")
+                .queryParam("id_token", jwtToken).encode().toUriString();
+
+        String resultJson = restTemplate.getForObject(requestUrl, String.class);
+
+        return mapper.readValue(resultJson, new TypeReference<Map<String, String>>(){});
+    }
+
 }
