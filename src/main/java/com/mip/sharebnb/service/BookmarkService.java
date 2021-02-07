5aa800791d1f6663a486999c1bc8280a1602618d
Tezla-Lee <tezla.lee@gmail.com>
2021-02-07 13:24:33 +0900

    feat: Update BookmarkService
    Add findBookmarks

--- src/main/java/com/mip/sharebnb/service/BookmarkService.java
+++ src/main/java/com/mip/sharebnb/service/BookmarkService.java
@@ -1,11 +1,19 @@
 package com.mip.sharebnb.service;
 
+import com.mip.sharebnb.model.Bookmark;
 import com.mip.sharebnb.repository.BookmarkRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BookmarkService {
     private final BookmarkRepository bookmarkRepository;
+
+    public List<Bookmark> findBookmarks(long memberId) {
+
+        return bookmarkRepository.findBookmarksByMember_Id(memberId);
+    }
 }
