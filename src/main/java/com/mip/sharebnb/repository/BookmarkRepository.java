be17f38a5a464ec6a2b752170d84eeaff010dfb6
Tezla-Lee <tezla.lee@gmail.com>
2021-02-07 13:23:55 +0900

    feat: Update BookmarkRepository
    Add findBookmarksByMember_Id

--- src/main/java/com/mip/sharebnb/repository/BookmarkRepository.java
+++ src/main/java/com/mip/sharebnb/repository/BookmarkRepository.java
@@ -3,5 +3,9 @@ package com.mip.sharebnb.repository;
 import com.mip.sharebnb.model.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+    List<Bookmark> findBookmarksByMember_Id(long memberId);
 }
