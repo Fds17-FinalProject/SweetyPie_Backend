822f4b188d0f5343aff7ba37b0855d0290e2572e
Tezla-Lee <tezla.lee@gmail.com>
2021-02-07 13:23:42 +0900

    feat: Update BookmarkController
    Add getBookmarks

--- src/main/java/com/mip/sharebnb/controller/BookmarkController.java
+++ src/main/java/com/mip/sharebnb/controller/BookmarkController.java
@@ -1,13 +1,27 @@
 package com.mip.sharebnb.controller;
 
+import com.mip.sharebnb.model.Bookmark;
+import com.mip.sharebnb.service.BookmarkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin
 @RequestMapping("/api")
 public class BookmarkController {
+
+    private final BookmarkService bookmarkService;
+
+    @GetMapping("/bookmark/{memberId}")
+    public List<Bookmark> getBookmarks(@PathVariable long memberId) {
+
+        return bookmarkService.findBookmarks(memberId);
+    }
 }
