deadfb4c7f5bdac63dabd3855e37838409164ebb
Tezla-Lee <tezla.lee@gmail.com>
2021-02-08 15:24:31 +0900

    docs: Delete data.sql

--- src/test/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into member(member_id, email, password, name, contact, birth_date)
-values (1, 'ddd@gmail.com', '1234', '이재복', '010-1234-1234', '1993-05-01');
-
-insert into accommodation(accommodation_id, city, gu)
-values (1, '서울특별시', '마포구');
-
-insert into bookmark(id, accommodation_id, member_id)
-values (1, 1, 1)
