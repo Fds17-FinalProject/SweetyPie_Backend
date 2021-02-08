73a460bdeb98345b4065f8b1ceaf46b8907425fa
FoeverNa <foevna@naver.com>
2021-02-08 12:43:39 +0900

    test: setting for deploy test

--- src/test/java/com/mip/sharebnb/SharebnbApplicationTests.java
+++ src/test/java/com/mip/sharebnb/SharebnbApplicationTests.java
@@ -3,9 +3,12 @@ package com.mip.sharebnb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//@SpringBootTest(properties = "spring.config.location="
+//        + "classpath:application.yml,"
+//        + "classpath:datasource.yml")
 @SpringBootTest(properties = "spring.config.location="
         + "classpath:application.yml,"
-        + "classpath:datasource.yml")
+        + "/home/ubuntu/conf/datasource.yml")
 class SharebnbApplicationTests {
 
     @Test
