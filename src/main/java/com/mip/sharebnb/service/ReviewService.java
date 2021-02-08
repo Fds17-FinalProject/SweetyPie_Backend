14e0c18e0579408677425b9e8aec11cb472fa109
Tezla-Lee <tezla.lee@gmail.com>
2021-02-08 15:20:50 +0900

    feat: Update ReviewService
    Add writeReview

--- src/main/java/com/mip/sharebnb/service/ReviewService.java
+++ src/main/java/com/mip/sharebnb/service/ReviewService.java
@@ -1,10 +1,15 @@
 package com.mip.sharebnb.service;
 
+import com.mip.sharebnb.dto.ReviewDto;
 import com.mip.sharebnb.model.Review;
+import com.mip.sharebnb.repository.AccommodationRepository;
+import com.mip.sharebnb.repository.MemberRepository;
 import com.mip.sharebnb.repository.ReviewRepository;
+import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Service
@@ -13,8 +18,27 @@ public class ReviewService {
 
     private final ReviewRepository reviewRepository;
 
+    private final AccommodationRepository accommodationRepository;
+
+    private final MemberRepository memberRepository;
+
     public Optional<Review> findReviewByAccommodation_IdAndMember_Id(long accommodationId, long memberId) {
 
         return reviewRepository.findReviewByAccommodation_IdAndMember_Id(accommodationId, memberId);
+    }
+
+    public void writeReview(ReviewDto reviewDto) throws NotFoundException {
+        Review review = Review.builder()
+                .content(reviewDto.getContent())
+                .createdDate(LocalDate.now())
+                .accommodation(accommodationRepository.findById(reviewDto.getAccommodationId())
+                        .orElseThrow(() -> new NotFoundException("Not Found Accommodation")))
+                .rating(reviewDto.getRating())
+                .member(memberRepository.findById(reviewDto.getMemberId())
+                        .orElseThrow(() -> new NotFoundException("Not Found Member")))
+                .build();
+
+
+        reviewRepository.save(review);
     }
 }
