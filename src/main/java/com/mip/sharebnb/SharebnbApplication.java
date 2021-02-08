46be2b7fa6a694a9b793fac0e2ff9aef4934c0ae
Tezla-Lee <tezla.lee@gmail.com>
2021-02-08 19:08:41 +0900

    release: Deploy

---
+++ src/main/java/com/mip/sharebnb/SharebnbApplication.java
@@ -0,0 +1,18 @@
+package com.mip.sharebnb;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class SharebnbApplication {
+
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "/home/ubuntu/conf/application.yml,"
+            + "/home/ubuntu/conf/datasource.yml";
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(SharebnbApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
+    }
+}
