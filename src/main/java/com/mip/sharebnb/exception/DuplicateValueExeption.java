a468e281953f527b8d734a8edb49431335f6a972
limjoonchul <dlawnscjf511@naver.com>
2021-02-10 01:06:37 +0900

    feat: Create DuplicateValueException

---
+++ src/main/java/com/mip/sharebnb/exception/DuplicateValueExeption.java
@@ -0,0 +1,7 @@
+package com.mip.sharebnb.exception;
+
+public class DuplicateValueExeption extends RuntimeException {
+    public DuplicateValueExeption(String message) {
+        super(message);
+    }
+}
