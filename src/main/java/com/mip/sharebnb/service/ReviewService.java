171b7fbd7a8e1c784adb83e9ead89065e6ef67e3
Tezla-Lee <tezla.lee@gmail.com>
2021-02-10 19:47:33 +0900

    feat: Update ReviewService
    Update writeReview

--- src/main/java/com/mip/sharebnb/service/ReviewService.java
+++ src/main/java/com/mip/sharebnb/service/ReviewService.java
@@ -3,9 +3,13 @@ package com.mip.sharebnb.service;
 import com.mip.sharebnb.dto.ReviewDto;
 import com.mip.sharebnb.exception.DataNotFoundException;
 import com.mip.sharebnb.exception.DuplicateValueExeption;
+import com.mip.sharebnb.model.Accommodation;
+import com.mip.sharebnb.model.Member;
+import com.mip.sharebnb.model.Reservation;
 import com.mip.sharebnb.model.Review;
 import com.mip.sharebnb.repository.AccommodationRepository;
 import com.mip.sharebnb.repository.MemberRepository;
+import com.mip.sharebnb.repository.ReservationRepository;
 import com.mip.sharebnb.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,6 +26,8 @@ public class ReviewService {
 
     private final MemberRepository memberRepository;
 
+    private final ReservationRepository reservationRepository;
+
     public Review findReviewByAccommodation_IdAndMember_Id(long accommodationId, long memberId) {
 
         return reviewRepository.findReviewByAccommodation_IdAndMember_Id(accommodationId, memberId)
@@ -34,13 +40,23 @@ public class ReviewService {
             throw new DuplicateValueExeption("이미 작성한 리뷰가 있습니다.");
         }
 
+        Reservation reservation = reservationRepository.findById(reviewDto.getReservationId())
+                .orElseThrow(() -> new DataNotFoundException("Reservation Not Found"));
+
+        Accommodation accommodation = accommodationRepository.findById(reviewDto.getAccommodationId())
+                .orElseThrow(() -> new DataNotFoundException("Accommodation Not Found"));
+
+        Member member = memberRepository.findById(reviewDto.getMemberId())
+                .orElseThrow(() -> new DataNotFoundException("Member Not Found"));
+
+        reservation.setIsWrittenReview(true);
+
         Review review = new Review();
         review.setRating(reviewDto.getRating());
         review.setContent(reviewDto.getContent());
-        review.setAccommodation(accommodationRepository.findById(reviewDto.getAccommodationId())
-                .orElseThrow(() -> new DataNotFoundException("Accommodation Not Found")));
-        review.setMember(memberRepository.findById(reviewDto.getMemberId())
-                .orElseThrow(() -> new DataNotFoundException("Member Not Found")));
+        review.setAccommodation(accommodation);
+        review.setMember(member);
+        review.setReservation(reservation);
 
         reviewRepository.save(review);
     }
