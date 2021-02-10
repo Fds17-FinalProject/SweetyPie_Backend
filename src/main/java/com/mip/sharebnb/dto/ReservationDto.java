9c91cf2da587f01c0d20fba64c4706d1d1d8bc1f
limjoonchul <dlawnscjf511@naver.com>
2021-02-10 17:48:39 +0900

    feat: Add ExceptionMessage

--- src/main/java/com/mip/sharebnb/dto/ReservationDto.java
+++ src/main/java/com/mip/sharebnb/dto/ReservationDto.java
@@ -3,6 +3,8 @@ package com.mip.sharebnb.dto;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Column;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Positive;
 import java.time.LocalDate;
@@ -28,11 +30,11 @@ public class ReservationDto {
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate checkoutDate;
 
-    @Positive
-    @Min(1)
+    @Positive(message = "인원수가 맞지 않습니다.")
+    @Min(value = 1, message = "인원은 최소 1명입니다.") @Max(value = 8, message = "인원은 최대 8명입니다.")
     private int guestNum;
 
-    @Positive
+    @Positive(message = "총 비용이 맞지 않습니다.")
     private int totalPrice;
 
     private Boolean isWrittenReview;
