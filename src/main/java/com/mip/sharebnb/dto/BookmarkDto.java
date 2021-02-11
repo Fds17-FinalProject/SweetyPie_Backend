cff8b036dea18a2e6207839a6c169d07ffed356a
Tezla-Lee <tezla.lee@gmail.com>
2021-02-11 18:28:23 +0900

    feat: Create BookmarkDto
    For posting

--- src/main/java/com/mip/sharebnb/dto/BookmarkDto.java
+++ src/main/java/com/mip/sharebnb/dto/BookmarkDto.java
@@ -1,4 +1,24 @@
 package com.mip.sharebnb.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Positive;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookmarkDto {
+
+    @Positive
+    private long bookmarkId;
+
+    @Positive
+    private long memberId;
+
+    @Positive
+    private long accommodationId;
 }
