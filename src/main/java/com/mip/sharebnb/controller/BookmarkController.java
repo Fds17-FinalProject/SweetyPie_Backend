b2a04054cd669410fe81f537c26feae2d4f74829
Tezla-Lee <tezla.lee@gmail.com>
2021-02-11 18:28:03 +0900

    feat: Update BookmarkController
    Add postBookmark
    Modify deleteBookmark

--- src/main/java/com/mip/sharebnb/controller/BookmarkController.java
+++ src/main/java/com/mip/sharebnb/controller/BookmarkController.java
@@ -1,15 +1,19 @@
 package com.mip.sharebnb.controller;
 
-import com.mip.sharebnb.model.Bookmark;
+import com.mip.sharebnb.dto.BookmarkDto;
 import com.mip.sharebnb.service.BookmarkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -21,14 +25,20 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping("/bookmark/{memberId}")
-    public List<Bookmark> getBookmarks(@PathVariable long memberId) {
+    public List<BookmarkDto> getBookmarks(@PathVariable long memberId) {
 
         return bookmarkService.findBookmarks(memberId);
     }
 
-    @DeleteMapping("/bookmark/{id}")
-    public void deleteBookmark(@PathVariable long id) {
+    @PostMapping("/bookmark")
+    public void postBookmark(@Valid @RequestBody BookmarkDto bookmarkDto) {
 
-        bookmarkService.deleteBookmarkById(id);
+        bookmarkService.postBookmark(bookmarkDto);
+    }
+
+    @DeleteMapping("/bookmark")
+    public void deleteBookmark(@RequestParam long memberId, @RequestParam long accommodationId) {
+
+        bookmarkService.deleteBookmark(memberId, accommodationId);
     }
 }
