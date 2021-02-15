20d81fc50eeadb42584269ddc1d7ac2ce9a41d37
limjoonchul <dlawnscjf511@naver.com>
2021-02-15 18:54:47 +0900

    refactor: Remove setAccommodation & setReservation

--- src/test/java/com/mip/sharebnb/repository/dynamic/DynamicReservationRepositoryTest.java
+++ src/test/java/com/mip/sharebnb/repository/dynamic/DynamicReservationRepositoryTest.java
@@ -1,8 +1,6 @@
 package com.mip.sharebnb.repository.dynamic;
 
-import com.mip.sharebnb.model.Accommodation;
 import com.mip.sharebnb.model.BookedDate;
-import com.mip.sharebnb.model.Reservation;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,13 +11,11 @@ import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @Rollback
 @Transactional
 @SpringBootTest(properties = "spring.config.location="
-        + "classpath:application.yml,"
-        + "classpath:datasource.yml")
+        + "classpath:test.yml")
 class DynamicReservationRepositoryTest {
 
     @Autowired
@@ -28,43 +24,18 @@ class DynamicReservationRepositoryTest {
     @Test
     void duplicateReservationDate(){
 
-        List<BookedDate> findBookedDate = dynamicReservationRepository.findByAccommodationIdAndDate(1L, LocalDate.of(2020, 2, 20), LocalDate.of(2020, 2, 22));
+        List<BookedDate> findBookedDate = dynamicReservationRepository.findByAccommodationIdAndDate(1L, LocalDate.of(2022, 2, 10), LocalDate.of(2022, 2, 12));
 
-        assertThat(findBookedDate.size()).isEqualTo(3);
-        assertThat(findBookedDate.get(0).getDate()).isEqualTo("2020-02-20");
-        assertThat(findBookedDate.get(1).getDate()).isEqualTo("2020-02-21");
-        assertThat(findBookedDate.get(2).getDate()).isEqualTo("2020-02-22");
+        assertThat(findBookedDate.size()).isEqualTo(2);
+        assertThat(findBookedDate.get(0).getDate()).isEqualTo("2022-02-10");
+        assertThat(findBookedDate.get(1).getDate()).isEqualTo("2022-02-11");
     }
 
     @Test
     void nonDuplicateReservationDate(){
 
-        List<BookedDate> findBookedDate = dynamicReservationRepository.findByAccommodationIdAndDate(1L, LocalDate.of(2020, 5, 20), LocalDate.of(2020, 5, 22));
+        List<BookedDate> findBookedDate = dynamicReservationRepository.findByAccommodationIdAndDate(1L, LocalDate.of(2022, 5, 20), LocalDate.of(2022, 5, 22));
 
         assertThat(findBookedDate.size()).isEqualTo(0);
     }
-
-    private Accommodation setAccommodation(){
-        Accommodation accommodation = new Accommodation();
-
-        accommodation.setId(1L);
-        accommodation.setCity("서울시");
-        accommodation.setGu("강남구");
-
-        return accommodation;
-    }
-
-
-    private Reservation setReservation(){
-
-        Reservation reservation = new Reservation();
-
-        reservation.setCheckInDate(LocalDate.of(2020, 2, 20));
-        reservation.setCheckoutDate(LocalDate.of(2020, 2, 22));
-        reservation.setGuestNum(3);
-        reservation.setTotalPrice(300000);
-
-        return reservation;
-    }
-
 }
