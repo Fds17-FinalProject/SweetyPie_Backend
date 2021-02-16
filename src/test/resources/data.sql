ae354a23960cbecbb31c59140c84abf863748cbe
limjoonchul <dlawnscjf511@naver.com>
2021-02-16 12:06:17 +0900

    refactor: Modify reservation query

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -7,13 +7,13 @@ INSERT INTO accommodation (id, city, gu, title, accommodation_desc, accommodatio
 values (1, '서울특별시', '마포구', '깨끗한 아파트', '깔끔해요', '전체', 'Mapo-gu, Seoul', 2, 2, 2, '아파트', 4, '010-1234-1234', '좋아요',
         '이재복', 0, 37, '마포역 5분거리', 126, 40000, 0, 0, '마포역, 대흥역, 공덕역', '12345');
 
-INSERT INTO reservation (id, `check_in_date`, `checkout_date`, `guest_num`, `is_canceled`, `is_written_review`,
+INSERT INTO reservation (id, `check_in_date`, `checkout_date`, `total_guest_num`, `adults`, `children`, `infants`, `is_canceled`, `is_written_review`,
                          `payment_date`, `total_price`, `accommodation_id`, `member_id`)
-VALUES (1, '2022-02-10', '2022-02-12', '3', 0, 1, '2022-03-01', 60000, 1, 1);
+VALUES (1, '2022-02-10', '2022-02-12', '3', '2', '2', '0', 0, 1, '2022-03-01', 60000, 1, 1);
 
-INSERT INTO reservation (id, `check_in_date`, `checkout_date`, `guest_num`, `is_canceled`, `is_written_review`,
+INSERT INTO reservation (id, `check_in_date`, `checkout_date`, `total_guest_num`, `adults`, `children`, `infants`, `is_canceled`, `is_written_review`,
                          `payment_date`, `total_price`, `accommodation_id`, `member_id`)
-VALUES (2, '2022-02-20', '2022-02-22', '3', 0, 0, '2022-03-03', 120000, 1, 1);
+VALUES (2, '2022-02-20', '2022-02-22', '3', '2', '3', '1', 0, 0, '2022-03-03', 120000, 1, 1);
 
 INSERT INTO review(id, `rating`, `content`, `accommodation_id`, `member_id`, `reservation_id`)
 values (1, 4, 'new content', 1, 1, 1);
