aed9412b5a5083b64ebe40e3a8969c7e6ba6f723
Tezla-Lee <tezla.lee@gmail.com>
2021-02-16 19:52:35 +0900

    refactorL Update data.sql
    Accommodation 1 -> 3

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -7,6 +7,18 @@ INSERT INTO accommodation (id, city, gu, title, accommodation_desc, accommodatio
 values (1, '서울특별시', '마포구', '깨끗한 아파트', '깔끔해요', '전체', 'Mapo-gu, Seoul', 2, 2, 2, '아파트', 4, '010-1234-1234', '좋아요',
         '이재복', 0, 37, '마포역 5분거리', 126, 40000, 0, 0, '마포역, 대흥역, 공덕역', '12345');
 
+INSERT INTO accommodation (id, city, gu, title, accommodation_desc, accommodation_type, address, bathroom_num, bed_num,
+                           bedroom_num, building_type, capacity, contact, host_desc, host_name, host_review_num,
+                           latitude, location_desc, longitude, price, rating, review_num, transportation_desc, rand_id)
+values (2, '서울특별시', '서대문구', '깨끗한 아파트', '깔끔해요', '전체', 'Seodaemun-gu, Seoul', 2, 2, 2, '아파트', 4, '010-1234-1234', '좋아요',
+        '이재복', 0, 37, '신촌역 5분거리', 126, 40000, 0, 0, '신촌역, 이대역', '12345');
+
+INSERT INTO accommodation (id, city, gu, title, accommodation_desc, accommodation_type, address, bathroom_num, bed_num,
+                           bedroom_num, building_type, capacity, contact, host_desc, host_name, host_review_num,
+                           latitude, location_desc, longitude, price, rating, review_num, transportation_desc, rand_id)
+values (3, '서울특별시', '성동구', '깨끗한 아파트', '깔끔해요', '전체', 'Sungdong-gu, Seoul', 2, 2, 2, '아파트', 4, '010-1234-1234', '좋아요',
+        '이재복', 0, 37, '성수역 5분거리', 126, 40000, 0, 0, '건대입구역, 성수역', '12345');
+
 INSERT INTO reservation (id, `check_in_date`, `checkout_date`, `total_guest_num`, `adult_num`, `child_num`, `infant_num`, `is_written_review`,
                          `payment_date`, `total_price`, `accommodation_id`, `member_id`)
 VALUES (1, '2022-02-10', '2022-02-12', '3', '2', '2', '0', 1, '2022-03-01', 60000, 1, 1);
@@ -30,7 +42,7 @@ INSERT INTO bookmark(id, accommodation_id, member_id)
 values (1, 1, 1);
 
 INSERT INTO bookmark(id, accommodation_id, member_id)
-values (2, 1, 1);
+values (2, 2, 1);
 
 INSERT INTO bookmark(id, accommodation_id, member_id)
-values (3, 1, 1);
+values (3, 3, 1);
