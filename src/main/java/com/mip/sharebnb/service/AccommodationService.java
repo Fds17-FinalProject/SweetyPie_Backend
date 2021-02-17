5debc73263be876d11de83188a7341669a93dfa3
Tezla-Lee <tezla.lee@gmail.com>
2021-02-18 00:52:49 +0900

    refactor: Update AccommodationService
    Add HttpServletRequest parameter

--- src/main/java/com/mip/sharebnb/service/AccommodationService.java
+++ src/main/java/com/mip/sharebnb/service/AccommodationService.java
@@ -1,17 +1,20 @@
 package com.mip.sharebnb.service;
 
 import com.mip.sharebnb.dto.AccommodationDto;
+import com.mip.sharebnb.dto.SearchAccommodationDto;
 import com.mip.sharebnb.exception.DataNotFoundException;
 import com.mip.sharebnb.exception.InvalidInputException;
 import com.mip.sharebnb.model.Accommodation;
 import com.mip.sharebnb.repository.AccommodationRepository;
 import com.mip.sharebnb.repository.dynamic.DynamicAccommodationRepository;
+import com.mip.sharebnb.security.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
 
 @Transactional
@@ -21,6 +24,7 @@ public class AccommodationService {
 
     private final DynamicAccommodationRepository dynamicAccommodationRepository;
     private final AccommodationRepository accommodationRepository;
+    private final TokenProvider tokenProvider;
 
     public AccommodationDto findById(Long id) {
         Accommodation accommodation = accommodationRepository.findById(id)
@@ -44,9 +48,9 @@ public class AccommodationService {
         return accommodationRepository.findByBuildingTypeContainingOrderByRandId(buildingType, page);
     }
 
-    public Page<Accommodation> findAccommodationsBySearch(String searchKeyword,
-                                                          LocalDate checkIn, LocalDate checkout,
-                                                          int guestNum, Pageable page) {
+    public Page<SearchAccommodationDto> findAccommodationsBySearch(HttpServletRequest request, String searchKeyword,
+                                                                   LocalDate checkIn, LocalDate checkout,
+                                                                   int guestNum, Pageable page) {
 
         if (checkIn != null && checkout != null && checkout.isBefore(checkIn)) {
             throw new InvalidInputException("Checkout time is before check-in time");
@@ -64,13 +68,13 @@ public class AccommodationService {
             checkIn = LocalDate.now();
         }
 
-        return dynamicAccommodationRepository.findAccommodationsBySearch(searchKeyword, checkIn, checkout, guestNum, page);
+        return dynamicAccommodationRepository.findAccommodationsBySearch(searchKeyword, checkIn, checkout, guestNum, parseRequestToMemberId(request), page);
     }
 
-    public Page<Accommodation> findAccommodationsByMapSearch(float minLatitude, float maxLatitude,
-                                                             float minLongitude, float maxLongitude, Pageable page) {
+    public Page<SearchAccommodationDto> findAccommodationsByMapSearch(HttpServletRequest request, float minLatitude, float maxLatitude,
+                                                                      float minLongitude, float maxLongitude, Pageable page) {
 
-        return dynamicAccommodationRepository.findAccommodationsByMapSearch(minLatitude, maxLatitude, minLongitude, maxLongitude, page);
+        return dynamicAccommodationRepository.findAccommodationsByMapSearch(minLatitude, maxLatitude, minLongitude, maxLongitude, parseRequestToMemberId(request), page);
     }
 
     private AccommodationDto mappingAccommodationDto(Accommodation accommodation) {
@@ -88,5 +92,19 @@ public class AccommodationService {
                 accommodation.getHostDesc(), accommodation.getHostReviewNum(),
                 accommodation.getReviews(), accommodation.getBookedDates(),
                 accommodation.getAccommodationPictures());
+    }
+
+    private Long parseRequestToMemberId(HttpServletRequest request) {
+        Long memberId = null;
+
+        if (request != null) {
+            String token = request.getHeader("Authorization");
+
+            if (token != null) {
+                memberId = tokenProvider.parseTokenToGetUserId(token);
+            }
+        }
+
+        return memberId;
     }
 }
