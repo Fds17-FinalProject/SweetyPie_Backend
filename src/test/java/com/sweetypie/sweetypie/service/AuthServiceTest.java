f2905bd74cc21801dc9351ca69fc591acb306067
FoeverNa <foevna@naver.com>
2021-02-21 19:30:12 +0900

    test : Add test Name

--- src/test/java/com/sweetypie/sweetypie/service/AuthServiceTest.java
+++ src/test/java/com/sweetypie/sweetypie/service/AuthServiceTest.java
@@ -2,6 +2,8 @@ package com.sweetypie.sweetypie.service;
 
 import com.sweetypie.sweetypie.dto.GoogleMemberDto;
 import com.sweetypie.sweetypie.dto.LoginDto;
+import com.sweetypie.sweetypie.model.Member;
+import com.sweetypie.sweetypie.repository.MemberRepository;
 import com.sweetypie.sweetypie.security.jwt.TokenProvider;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,11 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.core.Authentication;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -51,10 +53,7 @@ class AuthServiceTest {
     @Test
     void logoutTest() {
         String token = "ThisIsTestToken1234";
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        request.addHeader("Authorization", token);
-
-        authService.logout(request);
+        authService.logout(token);
 
         ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
         String result = valueOperations.get(token);
@@ -62,6 +61,7 @@ class AuthServiceTest {
         assertThat(result).isEqualTo(token);
     }
 
+    @DisplayName("로그인테스트")
     @Test
     void loginServiceTest() {
 
